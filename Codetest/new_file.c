6e37c7f1ceb1b4aea5d5c90202a3a37d07cbe6b7
hieutrung261199 <trunghieu41048@gmail.com>
2024-07-02 01:00:50 +0700

    Code for file

---
+++ Codetest/new_file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+    
+    const char *directory = "/etc/init.d/";
+
+    
+    const char *filename = "test_file.txt";
+
+    // Đường dẫn đầy đủ của file
+    char filepath[256];
+    snprintf(filepath, sizeof(filepath), "%s%s", directory, filename);
+
+    // Mở file để tạo mới, nếu file không tồn tại sẽ tạo mới, cờ O_CREAT
+    int fd = open(filepath, O_WRONLY | O_CREAT, 0644);
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    
+    const char *content = "Hello, this is a test file.\n";
+    ssize_t bytes_written = write(fd, content, strlen(content));
+    if (bytes_written == -1) {
+        perror("write");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    
+    if (close(fd) == -1) {
+        perror("close");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("File '%s' created successfully.\n", filepath);
+
+    return 0;
+}
