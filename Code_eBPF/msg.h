97c4f9fd958c0f0964b99353a3c713e2dcc9c0f9
Hieu Trung <144328421+HieuTrung261199@users.noreply.github.com>
2024-07-05 03:18:41 +0700

    Update msg.h

--- Code_eBPF/msg.h
+++ Code_eBPF/msg.h
@@ -1,10 +1,24 @@
 #ifndef __MSG_H__
 #define __MSG_H__
 
-struct my_msg {
+struct execve {
+    pid_t pid;
+    char command[128];
+    char pathname[128];
+    char filename[32];
+};
+
+struct add_file {
     pid_t pid;
     char command[128];
     char pathname[128];
 };
 
+struct content {
+    pid_t pid;
+    unsigned int fd;
+    const char * buf;
+    size_t count;
+    loff_t pos;
+};
 #endif // __MSG_H__
