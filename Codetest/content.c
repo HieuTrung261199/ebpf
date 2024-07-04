4241f5e36425bd7d6313440ea34ba7a254ed18ed
Hieu Trung <144328421+HieuTrung261199@users.noreply.github.com>
2024-07-05 03:19:20 +0700

    Create content.c

---
+++ Codetest/content.c
@@ -0,0 +1,16 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    int fd = open("text.txt", O_WRONLY | O_CREAT, 0644);
+    if (fd == -1) {
+        return 1;
+    }
+
+    const char *buf = "Hello, bpftrace!";
+    pwrite(fd, buf, strlen(buf), 15); // Ghi tại vị trí 0
+
+    close(fd);
+    return 0;
+}
